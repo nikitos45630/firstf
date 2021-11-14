fe57ac43428b37ae52cf35152e80bd8636c2570a
Admin <Boss.Baydin@list.ru>
2021-11-14 20:46:59 +0300

    cycles commit

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log(6);
+"use strict";
+
+
+
+
+
+
+const numberOfFilms = +prompt("сколько фильмов вы уже посмотрели?", "");
+
+const PersonalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("один из последних просмотренных фильмов", ""),
+          b = prompt("На сколько оцените его?","");
+        
+       
+        if (a != null && b != null && a != '' && b != '' && a.lenght < 50){
+
+    
+    
+          PersonalMovieDB.movies[a] = b;
+          console.log('done');
+
+        } else {
+            console.log('error');
+            i--;
+        }
+
+    }
+
+
+
+if(PersonalMovieDB.count < 10){
+    console.log("Просмотрено двольно мало фильмов");
+} else if (PersonalMovieDB.count >= 10 && PersonalMovieDB < 30){
+    console.log("Вы классический зритель");
+} else if (PersonalMovieDB.count >= 30){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(PersonalMovieDB);
